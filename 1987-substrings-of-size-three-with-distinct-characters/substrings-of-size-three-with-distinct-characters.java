95b8458a07e479775564d6bbca0f57f7261557c9
Aaditya Raj <ar912739@gmail.com>
2026-02-10 18:38:37 +0530

    Time: 5 ms (8.13%) | Memory: 44 MB (18.65%) - LeetSync

--- 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.java
+++ 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.java
@@ -1,35 +1,29 @@
-import java.util.HashMap;
-
 class Solution {
     public int countGoodSubstrings(String s) {
-        HashMap<Character, Integer> map = new HashMap<>();
+        HashMap<Character,Integer> map = new HashMap<>();
         int i = 0;
         int n = s.length();
         int j = 0;
         int count = 0;
-        int freq = 0;
-
-        while (j < n) {
-            char ch = s.charAt(j);
-            map.put(ch, map.getOrDefault(ch, 0) + 1);
-
-            // If window size exceeds 3, shrink from left
-            if (j - i + 1 > 3) {
-                char left = s.charAt(i);
-                map.put(left, map.get(left) - 1);
-                if (map.get(left) == 0) {
-                    map.remove(left);
+        int max = 0;
+        while(j < n){
+            if(!map.containsKey(s.charAt(j))){
+                map.put(s.charAt(j),1);
+            }else{
+                map.put(s.charAt(j),map.get(s.charAt(j))+1);
+            }
+            if(j - i + 1 > 3){
+                map.put(s.charAt(i),map.get(s.charAt(i))-1);
+                if(map.get(s.charAt(i)) == 0){
+                    map.remove(s.charAt(i));
                 }
                 i++;
             }
-
-            // Check window of size 3
-            if (j - i + 1 == 3 && map.size() == 3) {
-                freq++;
+            if(j - i + 1 == 3 && map.size() == 3){
+                count++;
             }
-
             j++;
         }
-        return freq;
+        return count;
     }
 }
